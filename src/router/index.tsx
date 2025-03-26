66133ebd7f445ec1bd4a8bdc6827471a9456fead
taese0ng <taese0ng@naver.com>
2025-03-26 16:21:03 +0900

    feat: 레이아웃 수정, framer-motion 추가, 아이콘 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,19 +2,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header, Footer } from "~/components/shared";
 import NotFound from "~pages/NotFound";
 import FAQ from "~pages/FAQ";
-
+import styled from "@emotion/styled";
 function Router() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<NotFound />} />
-        <Route path="/FAQ" element={<FAQ />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <BodyWrapper>
+        <Routes>
+          <Route path="/" element={<NotFound />} />
+          <Route path="/FAQ" element={<FAQ />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BodyWrapper>
       <Footer />
     </BrowserRouter>
   );
 }
 
 export default Router;
+
+const BodyWrapper = styled.div`
+  margin: auto;
+  max-width: 1240px;
+`;
