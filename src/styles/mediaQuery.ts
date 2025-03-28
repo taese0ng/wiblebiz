62db52f932d6a74b047d75ede3686e13baa09e5b
taese0ng <taese0ng@naver.com>
2025-03-28 14:28:28 +0900

    feat: mediaQuery 추가

--- src/styles/mediaQuery.ts
+++ src/styles/mediaQuery.ts
@@ -1,17 +1,21 @@
 interface Sizes {
   desktop: number;
+  tablet: number;
 }
 
 interface Media {
   mobile: string;
+  tablet: string;
   desktop: string;
 }
 
 export const BreakPoint: Sizes = {
   desktop: 1024,
+  tablet: 745,
 } as const;
 
 export const media: Media = {
-  mobile: `@media (max-width: ${BreakPoint.desktop}px)`,
+  mobile: `@media (max-width: ${BreakPoint.tablet}px)`,
+  tablet: `@media (max-width: ${BreakPoint.desktop}px)`,
   desktop: `@media (min-width: ${BreakPoint.desktop}px)`,
 } as const;
